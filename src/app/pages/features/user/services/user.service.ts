2a7efc67b39da1db24f97a6e7ffba76f1f25d324
Amir ullah <70256195+amirullah213@users.noreply.github.com>
2021-01-29 14:30:40 +0500

    changes

--- src/app/pages/features/user/services/user.service.ts
+++ src/app/pages/features/user/services/user.service.ts
@@ -171,44 +171,51 @@ export class UserService {
     downloadClientList(body: any): Observable<any> {
         const url = '/api/v1/ccm/DownloadUsersList/';
         return this.httpClient.post(APP_CONFIG.apiBaseUrl + url, body);
-      }
+    }
 
-      FetchTeam(body: any): Observable<any> {
-        const url = '/api/v1/uam/FetchTeam/';
-        return this.httpClient.post(APP_CONFIG.apiBaseUrl + url, body);
-      }
+    FetchTeam(body: any): Observable<any> {
+    const url = '/api/v1/uam/FetchTeam/';
+    return this.httpClient.post(APP_CONFIG.apiBaseUrl + url, body);
+    }
 
-      TeamList(body: any): Observable<any> {
-        const url = '/api/v1/uam/TeamList/';
-        return this.httpClient.post(APP_CONFIG.apiBaseUrl + url, body);
-      }
+    TeamList(body: any): Observable<any> {
+    const url = '/api/v1/uam/TeamList/';
+    return this.httpClient.post(APP_CONFIG.apiBaseUrl + url, body);
+    }
 
-      
-      TeamNameAvailability(body: any): Observable<any> {
-        const url = '/api/v1/uam/TeamNameAvailability/';
-        return this.httpClient.post(APP_CONFIG.apiBaseUrl + url, body);
-      }
-
-      AddTeam(body: any): Observable<any> {
-        const url = '/api/v1/uam/AddTeam/';
-        return this.httpClient.post(APP_CONFIG.apiBaseUrl + url, body);
-      }
-      EditTeam(body: any): Observable<any> {
-        const url = '/api/v1/uam/EditTeam/';
-        return this.httpClient.post(APP_CONFIG.apiBaseUrl + url, body);
-      }
-
-      DeleteTeam(body: any): Observable<any> {
-        const url = '/api/v1/uam/DeleteTeam/';
-        return this.httpClient.post(APP_CONFIG.apiBaseUrl + url, body);
-      }
-
-      TeamDropdownList(body: any): Observable<any> {
-        const url = '/api/v1/uam/TeamDropdownList/';
-        return this.httpClient.post(APP_CONFIG.apiBaseUrl + url, body);
-      }
     
+    TeamNameAvailability(body: any): Observable<any> {
+    const url = '/api/v1/uam/TeamNameAvailability/';
+    return this.httpClient.post(APP_CONFIG.apiBaseUrl + url, body);
+    }
+
+    AddTeam(body: any): Observable<any> {
+    const url = '/api/v1/uam/AddTeam/';
+    return this.httpClient.post(APP_CONFIG.apiBaseUrl + url, body);
+    }
+    EditTeam(body: any): Observable<any> {
+    const url = '/api/v1/uam/EditTeam/';
+    return this.httpClient.post(APP_CONFIG.apiBaseUrl + url, body);
+    }
+
+    DeleteTeam(body: any): Observable<any> {
+    const url = '/api/v1/uam/DeleteTeam/';
+    return this.httpClient.post(APP_CONFIG.apiBaseUrl + url, body);
+    }
+
+    TeamDropdownList(body: any): Observable<any> {
+    const url = '/api/v1/uam/TeamDropdownList/';
+    return this.httpClient.post(APP_CONFIG.apiBaseUrl + url, body);
+    }
+          
+    ClientDemographicDropdownList(): Observable<any> {
+        let url = APP_CONFIG.apiBaseUrl + "/api/v1/cmg/ClientDemographicDropdownList/";
+        return this.httpClient.get(url);
+    }
       
-      
-      
+    UserDropdownList(): Observable<any> {
+        let url = APP_CONFIG.apiBaseUrl + "/api/v1/uam/UserDropdownList/";
+        return this.httpClient.get(url);
+    }
+        
 }
