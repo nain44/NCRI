178379b81227a9432fb1ab2d77fa9783f4daee2c
Arshad Ali <aali@ozoqo.com>
2021-01-29 11:44:22 +0500

    arshad changes

--- src/app/pages/features/user-teams/edit-team/edit-team.component.ts
+++ src/app/pages/features/user-teams/edit-team/edit-team.component.ts
@@ -12,17 +12,19 @@ import { TeamService } from '../../user-teams/services/user-teams.service';
   styleUrls: ['./edit-team.component.scss']
 })
 export class EditTeamComponent implements OnInit {
-
   MANY_ITEMS = 'MANY_ITEMS';
-  public many = ['The', 'possibilities', 'are', 'endless!'];
-  public many2 = ['Explore', 'them'];
+  userList2 = [];
+  many2=["Please drag and drop a user from the list or select from the list"]
+ 
 
   subs = new Subscription();
   roleList: any = [];
   userList: any = [];
+  teamData:any={};
   userLoader = false;
   roleLoader = false;
-  gradeForm: FormGroup;
+  teamForm: FormGroup;
+  clinetList: any;
   responseText: any = [];
   modalRef: BsModalRef;
   @ViewChild('errorModal') errorModal: TemplateRef<any>;
@@ -33,39 +35,31 @@ export class EditTeamComponent implements OnInit {
   searchUsersText = '';
   sub: any;
   gradeID = '';
+  clientObj:any={};
+  clientObjE:any={};
+  selectedClientList:any=[];
+  showEditClient:boolean=false;
+  demId:any;
+  demName:any;
+  productList:any={}
   constructor(
-     private service: TeamService,
+    private service: TeamService,
     private dragulaService: DragulaService,
     private fb: FormBuilder,
     private modalService: BsModalService,
     private router: Router,
     private route: ActivatedRoute,
   ) {
-    this.gradeForm = this.fb.group({
+    this.teamForm = this.fb.group({
       name: ['', Validators.required],
       description: ['', Validators.required],
+      client_id: ['', Validators.required],
+      client_name: ['', Validators.required],
+      client_product: ['', Validators.required],
+      check:[""]
     });
 
-    this.subs.add(dragulaService.dropModel(this.MANY_ITEMS)
-      .subscribe(({ el, target, source, sourceModel, targetModel, item }) => {
-        console.log('dropModel:');
-        console.log(el);
-        console.log(source);
-        console.log(target);
-        console.log(sourceModel);
-        console.log(targetModel);
-        console.log(item);
-      })
-    );
-    this.subs.add(dragulaService.removeModel(this.MANY_ITEMS)
-      .subscribe(({ el, source, item, sourceModel }) => {
-        console.log('removeModel:');
-        console.log(el);
-        console.log(source);
-        console.log(sourceModel);
-        console.log(item);
-      })
-    );
+    
      // fetching query param from route
      this.sub = this.route
      .queryParams
@@ -76,16 +70,73 @@ export class EditTeamComponent implements OnInit {
        this.gradeID === '' ? this.router.navigate(['/user-teams']) : '';
      });
   }
-
+  
   ngOnDestroy() {
     this.subs.unsubscribe();
   }
 
   ngOnInit(): void {
-    this.getClientDemographicDropdownList();
     this.fetchTeam();
+    this.getUserDropdownList();
+    this.getClientDemographicDropdownList();
+    
   }
-
+  addToList(){
+    
+    this.subs.add(this.dragulaService.dropModel(this.MANY_ITEMS)
+      .subscribe(({ el, target, source, sourceModel, targetModel, item }) => {
+        debugger
+        console.log('dropModel:');
+        console.log(el);
+        console.log(source);
+        console.log(target);
+        console.log(sourceModel);
+        console.log(targetModel);
+        console.log(item);
+        console.log("userlist",this.userList2)
+      })
+    );
+}
+removeFromList(){
+  debugger
+  this.subs.add(this.dragulaService.removeModel(this.MANY_ITEMS)
+      .subscribe(({ el, source, item, sourceModel }) => {
+        
+        console.log('removeModel:');
+        console.log(el);
+        console.log(source);
+        console.log(sourceModel);
+        console.log(item);
+        
+      })
+    );
+ 
+    }
+ 
+    checkBx(usr){
+      this.userList2.push(usr)
+      this.userList.map(it => it.check = false);
+      console.log(this.userList2)
+      this.removeBx(usr)
+         }
+        removeBx(usr){
+          this.userList = this.userList.filter(item => item !== usr);
+          
+        }
+        checkBx2(usr2){
+          this.userList2 = this.userList2.filter(item => item !== usr2);
+          this.userList2.unshift(usr2)
+          console.log(this.userList)
+            }
+            addBack(usr2){
+              this.userList.unshift(usr2)
+              this.userList.map(it => it.check = false);
+              console.log(this.userList)
+                }
+            removeBx2(usr2){
+              this.userList2 = this.userList2.filter(item => item !== usr2);
+              this.addBack(usr2)
+            }
   openErrorModal(): void {
     this.modalRef = this.modalService.show(this.errorModal);
   }
@@ -95,19 +146,31 @@ export class EditTeamComponent implements OnInit {
     this.modalRef = this.modalService.show(deleteclient);
     // this.modalRef.content.userActivate = 'Close';
   }
-
-  
-  getClientDemographicDropdownList(): void {
+  getUserDropdownList(): void {
     this.roleLoader = true;
-    
-    this.service.ClientDemographicDropdownList().subscribe((res) => {
+    this.service.UserDropdownList().subscribe((res) => {
       if (res.status === 'success') {
-        this.roleList = res.data;
-        this.roleList.map(it => it.check = false);
+        this.userList = res.data;
+        this.userList.map(it => it.check = false);
       }
       this.roleLoader = false;
     }, (error) => {
       this.roleLoader = false;
+    });
+  }
+  
+  getClientDemographicDropdownList(): void {
+    
+    this.userLoader = true;
+   
+    this.service.ClientDemographicDropdownList().subscribe((res) => {
+      if (res.status === 'success') {
+        this.clinetList = res.data;
+        
+      }
+      this.userLoader = false;
+    }, (error) => {
+      this.userLoader = false;
     });
   }
 
@@ -118,17 +181,92 @@ export class EditTeamComponent implements OnInit {
     };
     this.service.FetchTeam(obj).subscribe((res) => {
       if (res.status === 'success') {
-        debugger
-        this.userList = res.data;
-        this.gradeForm.controls.name.setValue(this.userList.name);
-        this.userList.map(it => it.check = false);
+      
+        this.teamData = res.data;
+        this.clientObj.tName=this.teamData.name;
+        this.userList2=this.teamData.team_users;
+        console.log('json data',JSON.parse(this.teamData.team_client_demographics[0].product_list) );
+       
+        this.selectedClientList = this.teamData.team_client_demographics.map(it => ({
+          code: it.client_demographic__client_number,
+          name: it.client_demographic__company_name,
+          product: JSON.parse(it.product_list)[0] ? JSON.parse(it.product_list)[0] : "",
+          id: it.client_demographic_id
+        }));
+       // this.teamForm.controls.name.setValue(this.userList.name);
+        //this.userList.map(it => it.check = false);
       }
       this.userLoader = false;
     }, (error) => {
       this.userLoader = false;
     });
   }
+  //-------------------------------------
+  selectValues(value:any){
+    debugger
+    this.clientObj.client_id=value;
+    this.clientObj.client_name=value;
+    // this.teamForm.controls.product_list.setValue(value);
+    let index = this.selectedClientList.findIndex(it => it.id === value);
+    if(index >= 0){
+      // this.teamForm.controls.product_list.setValue([this.clientList[index].product]);
+      let obj = {
+        id:value,
+        name:this.selectedClientList[index].product
+      }
+      this.productList = [obj];
+    }
+    
+  }
+  //------------------------------------
+  showEdit(obj:any,indx){
+    debugger;
+    this.showEditClient=true;
+    this.clientObjE.client_idE=obj;
+    this.clientObjE.client_nameE=obj.name;
+    this.clientObjE.client_productE=obj.product;
+    this.clientObjE.index1=indx;
 
+
+
+  }
+  getClientInfo(event){
+    debugger;
+    this.demId=event.client_id.id;
+    this.demName=event.client_id.client_number;
+
+  }
+  addClients(obj:any){
+    debugger
+    this.selectedClientList.push({id:obj.client_id.id,code:obj.client_id.client_number,name:obj.client_name,product:obj.client_product})
+    this.clientObj={};
+  }
+  updateClient(objUpdate:any, indx){
+   debugger
+    this.showEditClient=false;
+    this.clientObjE={};
+    this.selectedClientList[indx].id=objUpdate.client_idE.id;
+    this.selectedClientList[indx].code=objUpdate.client_idE.client_number;
+    this.selectedClientList[indx].name=objUpdate.client_nameE;
+    this.selectedClientList[indx].product=objUpdate.client_productE;
+    
+    console.log("this.selectedClientList====",this.selectedClientList)
+  }
+
+   removeClient(obj:any){
+    this.selectedClientList = this.selectedClientList.filter(item => item !== obj);
+   }
+   updateUserTeam()
+    {
+      const obj = {
+        id: this.gradeID ,
+        name: this.clientObj.tName,
+        team_users:[{user_id:'333',is_team_lead:"ffff"}],
+        team_client_demographics:[{client_demographic_id:'hhhh',product_list:["dfdfsd","fasfsafs"]}]
+      }
+      }
+     
+  
   assignGradeToUser(obj: any): void {
     // this.service.assignGradeToUser(obj).subscribe((res) => {
     //   if (res.status === 'success') {
@@ -233,8 +371,8 @@ export class EditTeamComponent implements OnInit {
       return;
     }
     const obj = {
-      name: this.gradeForm.get('name').value,
-      description: this.gradeForm.get('description').value,
+      name: this.teamForm.get('name').value,
+      description: this.teamForm.get('description').value,
       user_role_ids: this.roleList.filter(it => it.check === true).map(it => it.id),
       user_ids: this.userList.filter(it => it.check === true).map(it => it.id)
     };
